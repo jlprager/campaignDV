fe9f083e9a7fa2cbdc0d1677a29c532c8e944cc8
Chuck Dishmon <chuckdishmon@gmail.com>
2015-12-29 20:21:19 -0500

    add sentiment scores and candidate tweets

--- public/javascript/controllers/CandidateController.js
+++ public/javascript/controllers/CandidateController.js
@@ -3,8 +3,15 @@
     angular.module('app')
     .controller('CandidateController', CandidateController);
 
-    function CandidateController() {
+    function CandidateController(TweetFactory) {
         var vm = this;
+
+        // Pass in candidate based on a future conditional $stateParams we set up. Hardcoded at moment for testing.
+        TweetFactory.getCandidateTweets('clinton').then(function(res) {
+            vm.tweets = res;
+        }, function(err) {
+            //
+        });
 
     }
 })();
--- server.js
+++ server.js
@@ -131,7 +131,7 @@ var waitForTweets = function(db) {
         sentiment(data.text, function(err, result) {
             // Create the tweet object
             var tweet = new Tweet({
-            	candidate: "Bernie",
+            	candidate: "bernie",
                 user: data.user.screen_name,
                 description: data.text,
                 sentiment: result.score,
@@ -152,7 +152,7 @@ var waitForTweets = function(db) {
         sentiment(data.text, function(err, result) {
             // Create the tweet object
             var tweet = new Tweet({
-            	candidate: "Cliton",
+            	candidate: "clinton",
                 user: data.user.screen_name,
                 description: data.text,
                 sentiment: result.score,
@@ -160,7 +160,7 @@ var waitForTweets = function(db) {
             });
 
             clintonCount++;
-            
+
             tweet.save(function(err, tweet) {
                 if (err) return console.error(err);
                 console.log(tweet.candidate + "(" + tweet.created_at + ") scored " + tweet.sentiment + ": " + tweet.description);
@@ -173,7 +173,7 @@ var waitForTweets = function(db) {
         sentiment(data.text, function(err, result) {
             // Create the tweet object
             var tweet = new Tweet({
-                candidate: "Trump",
+                candidate: "trump",
                 user: data.user.screen_name,
                 description: data.text,
                 sentiment: result.score,
@@ -195,7 +195,7 @@ var waitForTweets = function(db) {
         sentiment(data.text, function(err, result) {
             // Create the tweet object
             var tweet = new Tweet({
-                candidate: "Bush",
+                candidate: "bush",
                 user: data.user.screen_name,
                 description: data.text,
                 sentiment: result.score,
