649b73774e6d2a2140aae0f5b526e8334efc562e
Chuck Dishmon <chuckdishmon@gmail.com>
2015-12-30 18:28:19 -0500

    add timed polling of server

--- public/javascript/controllers/CandidateController.js
+++ public/javascript/controllers/CandidateController.js
@@ -3,15 +3,30 @@
     angular.module('app')
     .controller('CandidateController', CandidateController);
 
-    function CandidateController(TweetFactory) {
+    function CandidateController($scope, $timeout, TweetFactory) {
         var vm = this;
+        vm.tweets;
+        vm.timeoutHandler;
 
-        // Pass in candidate based on a future conditional $stateParams we set up. Hardcoded at moment for testing.
-        TweetFactory.getCandidateTweets('clinton').then(function(res) {
-            vm.tweets = res;
-        }, function(err) {
-            //
-        });
+        // Updating client side every 15 seconds(this function). Pulling from last 15 seconds of data(tweetRoutes.js)
+        (function tick() {
+            // Pass in candidate based on a future conditional $stateParams we set up. Hardcoded at moment for testing.
+            TweetFactory.getCandidateTweets('bernie').then(function(res) {
+                vm.tweets = res;
+                vm.timeoutHandler = $timeout(tick, 10000);
+            }, function(err) {
+				//
+			});
+    	})();
+
+        // Cancel interval on page change
+		$scope.$on('$destroy', function(){
+    		if (angular.isDefined(vm.timeoutHandler)) {
+        		$timeout.cancel(vm.timeoutHandler);
+        		vm.timeoutHandler = undefined;
+    		}
+		});
+
 
     }
 })();
--- public/javascript/controllers/HomeController.js
+++ public/javascript/controllers/HomeController.js
@@ -3,29 +3,47 @@
 	angular.module('app')
 	.controller('HomeController', HomeController);
 
-	function HomeController($state, $location, $timeout, UserFactory, TweetFactory) {
+	function HomeController($scope, $state, $location, $timeout, UserFactory, TweetFactory) {
 		var vm = this;
 		var url = $location.search();
 		var size = 0;
+		vm.timeoutHandler;
+		vm.bernie = 0;
+		vm.clinton = 0;
+		vm.trump = 0;
+		vm.bush = 0;
 
 		if(url.code){
 			UserFactory.setToken(url.code);
 			$location.search("code", null);
 		}
 
-		TweetFactory.getRecentTweets().then(function(res) {
-			vm.bernie = res.bernie;
-			vm.clinton = res.clinton;
-			vm.trump = res.trump;
-			vm.bush = res.bush;
+		// Updating client side every 15 seconds(this function). Pulling from last 5 minutes of data(tweetRoutes.js)
+		(function tick() {
+			TweetFactory.getRecentTweets().then(function(res) {
+				vm.bernie = res.bernie;
+				vm.clinton = res.clinton;
+				vm.trump = res.trump;
+				vm.bush = res.bush;
 
-			var x = 300;
-			var y = res.bernie;
-			x = (x + (x * y));
-			document.getElementById("this").height = x;
+				var x = 300;
+				var y = res.bernie;
+				x = (x + (x * y));
+				document.getElementById("this").height = x;
 
-		}, function(err) {
-			//
+				vm.timeoutHandler = $timeout(tick, 15000);
+
+			}, function(err) {
+				//
+			});
+    	})();
+
+		// Cancel interval on page change
+		$scope.$on('$destroy', function(){
+    		if (angular.isDefined(vm.timeoutHandler)) {
+        		$timeout.cancel(vm.timeoutHandler);
+        		vm.timeoutHandler = undefined;
+    		}
 		});
 
 	}
