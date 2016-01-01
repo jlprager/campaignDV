46ae887cdbed8405ed0a442fd0db20e5636bf634
Ben Doyle <benjaminjdoyle@gmail.com>
2016-01-01 12:37:28 -0800

    updated StripeController

--- public/javascript/controllers/StripeController.js
+++ public/javascript/controllers/StripeController.js
@@ -1,12 +1,14 @@
 (function() {
     'use strict';
-      angular.module("app", ['stripe.checkout'])
+      angular.module("app")
         .controller("StripeController", StripeController);
 
-        function StripeController() {
-          
-          this.doCheckout = function(token) {
-            alert("Got Stripe token: " + token.id);
+        function StripeController(UserFactory) {
+          var vm = this;
+
+          vm.stripeCallback = function(status, response) {
+            alert("Got Stripe token: " + response.id);
+            UserFactory.postCharge(response.id);
           };
         };
 })();
