e4a7092d232b2283646e433c1b882fe1239a0075
Ben Doyle <benjaminjdoyle@gmail.com>
2016-01-01 12:55:03 -0800

    added promise to stripe controller and UserFactory, added uuid to o.status in UserFactory and added Post Charge ajax call in UserFactory

--- public/javascript/controllers/StripeController.js
+++ public/javascript/controllers/StripeController.js
@@ -3,12 +3,14 @@
       angular.module("app")
         .controller("StripeController", StripeController);
 
-        function StripeController(UserFactory) {
+        function StripeController(UserFactory, $state) {
           var vm = this;
 
           vm.stripeCallback = function(status, response) {
             alert("Got Stripe token: " + response.id);
-            UserFactory.postCharge(response.id);
+            UserFactory.postCharge(response.id).then(function(res) {
+            	$state.go('Home');//add some kind of toast here 'Account upgraded to Premium Status'
+            });
           };
-        };
+        }
 })();
