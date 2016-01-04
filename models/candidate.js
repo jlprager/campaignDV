6d6ccf291a98a318c65aa4374781e0856b1ecb87
jlprager <jaclyn.prager@gmail.com>
2016-01-04 10:39:12 -0800

    completely rebuilt server and twitter stream (sentiment analysis changes, db collection changes), live tweets being pushed to client

--- models/candidate.js
+++ models/candidate.js
@@ -3,13 +3,7 @@ let mongoose = require("mongoose");
 
 let CandidateSchema = new mongoose.Schema({
 	name: String,
-	sentiment: {
-		tweets: [{  type : mongoose.Schema.Types.ObjectId, ref: "Tweet" }],
-		score: Number
-	},
-	dateCreated: { type : Date, default: Date.now },
-	user: { type : mongoose.Schema.Types.ObjectId, ref: "User" },
-	comments: [{ type : mongoose.Schema.Types.ObjectId, ref: "Comment" }]
+	sentiment: Number
 });
 
 module.exports = mongoose.model("Candidate", CandidateSchema);
--- public/javascript/controllers/CandidateController.js
+++ public/javascript/controllers/CandidateController.js
@@ -7,32 +7,36 @@
         var vm = this;
         vm.tweets;
         vm.timeoutHandler;
-        if($stateParams.id == '56856d3fb15a9ff859aa0784') vm.tweetStreamName = 'bernie';
-        if($stateParams.id == '56856d3fb15a9ff859aa0785') vm.tweetStreamName = 'clinton';
-        if($stateParams.id == '56856d47b15a9ff859aa0786') vm.tweetStreamName = 'bush';
-        if($stateParams.id == '56856d4fb15a9ff859aa0787') vm.tweetStreamName = 'trump';
+        var candidateName;
+        // console.log($stateParams.id);
+        // if($stateParams.id == '56856d3fb15a9ff859aa0784') vm.tweetStreamName = 'bernie';
+        // if($stateParams.id == '56856d3fb15a9ff859aa0785') vm.tweetStreamName = 'clinton';
+        // if($stateParams.id == '56856d47b15a9ff859aa0786') vm.tweetStreamName = 'bush';
+        // if($stateParams.id == '56856d4fb15a9ff859aa0787') vm.tweetStreamName = 'trump';
 
         CandidateFactory.getCandidateById($stateParams.id).then(function(res) {
             vm.candidate = res;
+            candidateName = res.name;
+            console.log(res);
         }, function(err) {
             //
         });
 
         (function tick() {
-            TweetFactory.getCandidateTweets(vm.tweetStreamName).then(function(res) {
+            TweetFactory.getCandidateTweets(candidateName).then(function(res) {
                 vm.tweets = res;
-                vm.timeoutHandler = $timeout(tick, 10000);
+                vm.timeoutHandler = $timeout(tick, 5000);
             }, function(err) {
                 //
             });
         })();
 
         // Cancel interval on page change
-		$scope.$on('$destroy', function(){
-    		if (angular.isDefined(vm.timeoutHandler)) {
-        		$timeout.cancel(vm.timeoutHandler);
-        		vm.timeoutHandler = undefined;
-    		}
-		});
+        $scope.$on('$destroy', function(){
+            if (angular.isDefined(vm.timeoutHandler)) {
+                $timeout.cancel(vm.timeoutHandler);
+                vm.timeoutHandler = undefined;
+            }
+        });
     }
 })();
