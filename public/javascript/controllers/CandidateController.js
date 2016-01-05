38f58536c1f60f2ae0e9391b88b54d7205677d7f
Chuck Dishmon <chuckdishmon@gmail.com>
2016-01-05 12:29:56 -0500

    change candidate timer to 1 second, remove old comments

--- public/javascript/controllers/CandidateController.js
+++ public/javascript/controllers/CandidateController.js
@@ -8,16 +8,10 @@
         vm.tweets;
         vm.timeoutHandler;
         var candidateName;
-        // console.log($stateParams.id);
-        // if($stateParams.id == '56856d3fb15a9ff859aa0784') vm.tweetStreamName = 'bernie';
-        // if($stateParams.id == '56856d3fb15a9ff859aa0785') vm.tweetStreamName = 'clinton';
-        // if($stateParams.id == '56856d47b15a9ff859aa0786') vm.tweetStreamName = 'bush';
-        // if($stateParams.id == '56856d4fb15a9ff859aa0787') vm.tweetStreamName = 'trump';
 
         CandidateFactory.getCandidateById($stateParams.id).then(function(res) {
             vm.candidate = res;
             candidateName = res.name;
-            console.log(candidateName);
         }, function(err) {
             //
         });
@@ -25,7 +19,7 @@
         (function tick() {
             TweetFactory.getCandidateTweets(candidateName).then(function(res) {
                 vm.tweets = res;
-                vm.timeoutHandler = $timeout(tick, 5000);
+                vm.timeoutHandler = $timeout(tick, 1000);
             }, function(err) {
                 //
             });
