01bc16885ccfb5df915977a3e4301cb4b66c3991
jlprager <jaclyn.prager@gmail.com>
2016-01-08 00:09:11 -0800

    testing registration alert

--- public/javascript/controllers/GlobalController.js
+++ public/javascript/controllers/GlobalController.js
@@ -1,28 +1,39 @@
 (function() {
     'use strict';
     angular.module('app')
-    .controller('GlobalController', GlobalController);
+        .controller('GlobalController', GlobalController);
 
     function GlobalController(UserFactory, $state) {
         var vm = this;
         vm.user = {};
         vm.status = UserFactory.status;
 
+        // vm.registerAlert = function() {
+        //     swal({
+        //         title: "",
+        //         text: '<div class="mainContent"><br><h1 style="text-align: center;margin-top: 5%;">Registration</h1><form id="registration" class="pure-form pure-form-aligned" ng-submit="register();"><fieldset><label>Email</label><div class="pure-control-group"><input id="email" type="email" ng-model="nav.user.email" placeholder="Email Required"></div><label>Password</label><div class="pure-control-group"><input id="password" type="password" ng-model="nav.user.password" placeholder="Password Required"></div><button ui-sref="Reg" type="submit" style="background-color: #82DD51;">Register</button></fieldset></form></div>',
+        //         html: true,
+        //         showConfirmButton: false
+        //     });
+        //     // vm.register();
+        //     // console.log(vm.user + "AAAAAA")
+        // };
+
+
         vm.register = function() {
-          UserFactory.register(vm.user).then(function(res) {
-            $state.go('Home');
-          });
+            UserFactory.register(vm.user).then(function(res) {
+            });
         };
 
         vm.login = function() {
-          UserFactory.login(vm.user).then(function(res) {
-            $state.go('Home');
-          });
+            UserFactory.login(vm.user).then(function(res) {
+                $state.go('Home');
+            });
         };
 
         vm.logout = function() {
-          UserFactory.removeToken();
-          $state.go('Home');
+            UserFactory.removeToken();
+            $state.go('Home');
         };
 
     }
